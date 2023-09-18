6c723440adf1d2a6dbf0d16da82f6edcb2cbea7b
yui <119509086+yuisofull@users.noreply.github.com>
2023-09-18 20:38:11 +0700

    Update asteroid-collision.go

--- asteroid-collision.go
+++ asteroid-collision.go
@@ -12,36 +12,15 @@ func asteroid(stack []int, v int) ([]int, int) {
                 return stack, v
             case i < 0:
                 stack, _ = asteroid(stack[:len(stack) - 1], v)
-                if (stack[len(stack) - 1] + v) < 0 {
-                    return append(stack, v), v
-                }
                 return stack, v 
-                
             default:
                 return stack, v
         }
       } else {
-        stack = append(stack, v)
-        return stack, v
+        return append(stack, v), v
       }
     } else {
-        if stack[len(stack) - 1] < 0 {
-            i := stack[len(stack) - 1] + v
-            switch {
-                case i == 0:
-                    stack = stack [:len(stack) - 1]
-                    return stack, v
-                case i > 0:
-                    stack, _ = asteroid(stack[:len(stack) - 1], v)
-                    if (stack[len(stack) - 1] + v) > 0 {
-                        return append(stack, v), v
-                    }
-                    return stack, v
-            }
-        } else {
-            stack = append(stack, v)
-            return stack, v
-      }
+        return append(stack, v), v
     }
     return stack, v
 }
