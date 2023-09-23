ef9d019d86ad0bd2a4748074801583a2af82e694
Your Name <you@example.com>
2023-09-23 22:01:30 +0700

    add https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree

---
+++ lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == p || root == q || root == nil {
+		return root
+	}
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+	if l != nil && r != nil {
+		return root
+	}
+	if l != nil {
+		return l
+	}
+	return r
+}
